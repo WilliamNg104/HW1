75b7a401333fbea3d7c41d20216ae256998c50a2
WilliamNg104 <114209234+WilliamNg104@users.noreply.github.com>
2022-10-06 22:59:07 +0900

    HW1

---
+++ HW1/HW 2.21/111/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  111
+//
+//  Created by William Ng on 2022/10/04.
+//
+
+#include <stdio.h>
+int main(int argc, const char *argv[]) {
+    printf("      *********          ***            *           *\n");
+    printf("      *       *       *      *         ***        *   *\n");
+    printf("      *       *      *        *       *****      *     *\n");
+    printf("      *       *      *         *        *       *       *\n");
+    printf("      *       *      *         *        *      *         *\n");
+    printf("      *       *      *         *        *       *       *\n");
+    printf("      *       *       *       *         *        *     *\n");
+    printf("      *       *        *     *          *         *  *\n");
+    printf("      *********          ***            *          *\n");
+}
---
+++ HW1/HW1 2.27/HW1 2.27/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  HW1 2.27
+//
+//  Created by William Ng on 2022/10/05.
+//
+
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf(" *******\n");
+    printf("*********\n");
+}
